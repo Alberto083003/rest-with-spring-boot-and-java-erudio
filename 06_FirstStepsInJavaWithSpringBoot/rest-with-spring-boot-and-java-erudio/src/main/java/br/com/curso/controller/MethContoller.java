54884603e2accc205150e623aaf685c6e634e93a
Alberto083003 <alka083003@gmail.com>
2022-10-11 09:34:37 -0300

    calculadora

---
+++ 06_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/curso/controller/MethContoller.java
@@ -0,0 +1,75 @@
+package br.com.curso.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.curso.converters.NumberConverter;
+import br.com.curso.exceptions.UnsupportedMathOperationException;
+import br.com.curso.math.SimpleMath;
+
+@RestController
+public class MethContoller {
+	
+	private final AtomicLong counter = new AtomicLong();
+	private SimpleMath math = new SimpleMath();
+	
+	@RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Pease set a numeric value!");
+        }
+        return math.sum(NumberConverter.covertToDouble(numberOne) , NumberConverter.covertToDouble(numberTwo));
+    }    
+	@RequestMapping(value="/subtraction/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+		public Double 
+		subtraction(@PathVariable("numberOne") String numberOne,
+			@PathVariable("numberTwo") String numberTwo) throws Exception {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Pease set a numeric value!");
+		}
+		return math.subtraction(NumberConverter.covertToDouble(numberOne) , NumberConverter.covertToDouble(numberTwo));
+	}    
+	@RequestMapping(value="/multplication/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double 
+	multplication(@PathVariable("numberOne") String numberOne,
+			@PathVariable("numberTwo") String numberTwo) throws Exception {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Pease set a numeric value!");
+		}
+		return math.multplication(NumberConverter.covertToDouble(numberOne) , NumberConverter.covertToDouble(numberTwo));
+	}    
+	@RequestMapping(value="/division/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double 
+	division(@PathVariable("numberOne") String numberOne,
+			@PathVariable("numberTwo") String numberTwo) throws Exception {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Pease set a numeric value!");
+		}
+		return math.division(NumberConverter.covertToDouble(numberOne) , NumberConverter.covertToDouble(numberTwo));
+	}    
+	@RequestMapping(value="/mean/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double 
+	mean(@PathVariable("numberOne") String numberOne,
+			@PathVariable("numberTwo") String numberTwo) throws Exception {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Pease set a numeric value!");
+		}
+		return math.mean(NumberConverter.covertToDouble(numberOne) , NumberConverter.covertToDouble(numberTwo));
+	}    
+	@RequestMapping(value="/squareRoot/{number}", method=RequestMethod.GET)
+	public Double 
+	squareRoot(@PathVariable("number") String number) throws Exception {
+		if (!NumberConverter.isNumeric(number)) {
+			throw new UnsupportedMathOperationException("Pease set a numeric value!");
+		}
+		return math.squareRoot(NumberConverter.covertToDouble(number));
+	}    
+    
+    
+
+}
